8ad21f786d832f178ef4b94906947cd0cb5ad73d
kirankm111 <110974093+kirankm111@users.noreply.github.com>
2023-08-14 16:32:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,82 +1,59 @@
-const express = require('express');
-const db = require('db')
-const cors = require('cors')
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
 
 const app = express();
 
-const  PORT = 3002;
+app.use(express.json());
 app.use(cors());
-app.use(express.json())
 
-// Route to get all posts
-app.get("/api/get", (req,res)=>{
-db.query("SELECT * FROM posts", (err,result)=>{
-    if(err) {
-    console.log(err)
-    } 
-res.send(result)
-}
-    );   
-});
-
-// Route to get one post
-app.get("/api/getFromId/:id", (req,res)=>{
-
-const id = req.params.id;
-    db.query("SELECT * FROM posts WHERE id = ?", id, (err,result)=>{
-        if(err) {
-        console.log(err)
-        } 
-    res.send(result)
-    }
-    );   
-    });
-
-// Route for creating the post
-app.post('/api/create', (req,res)=> {
-
-const username = req.body.userName;
-const title = req.body.title;
-const text = req.body.text;
-
-console.log(username,title,text)
-
-db.query("INSERT INTO posts (title, post_text, user_name) VALUES (?,?,?)",[title,text,username], (err,result)=>{
-   if(err) {
-    alert(err);
-       console.log(err)
-   } 
-   console.log(result)
-}
-);   
+const con = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "",
+    database: "register"
 })
-
-// Route for like
-app.post('/api/like/:id',(req,res)=>{
-
-const id = req.params.id;
-db.query("UPDATE posts SET likes = likes + 1 WHERE id = ?",id, (err,result)=>{
-    if(err) {
-    console.log(err)
-    } 
-    console.log(result)
-    }
-    );    
-});
-
-// Route to delete a post
-
-app.delete('/api/delete/:id',(req,res)=>{
-    const id = req.params.id;
-
-db.query("DELETE FROM posts WHERE id= ?", id, (err,result)=>{
-if(err) {
-console.log(err)
-        } 
-})
+app.get('/status', (req, res) => {
+    
+    res.send({message: "working2!"})
+    
 })
 
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on ${PORT}`)
+app.post('/register', (req, res) => {
+    const email = req.body.email;
+    const username = req.body.username;
+    const password = req.body.password;
+
+    con.query("INSERT INTO users (email, username, password) VALUES (?, ?, ?)", [email, username, password], 
+        (err, result) => {
+            if(result){
+                res.send(result);
+            }else{
+                res.send({message: "ENTER CORRECT ASKED DETAILS!"})
+            }
+        }
+    )
+})
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    con.query("SELECT * FROM users WHERE username = ? AND password = ?", [username, password], 
+        (err, result) => {
+            if(err){
+                req.setEncoding({err: err});
+            }else{
+                if(result.length > 0){
+                    res.send(result);
+                }else{
+                    res.send({message: "WRONG USERNAME OR PASSWORD!"})
+                }
+            }
+        }
+    )
+})
+
+app.listen(3001, () => {
+    console.log("running backend server");
 })
